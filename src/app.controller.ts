4ad0a01400e5b12a92eeea0d46aa4b049687b3a7
Junyoung Song <junyoungsong@Junyoungui-MacBookAir.local>
2022-05-09 18:50:44 +0900

    :spakles: edit / docker 관련 파일

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,9 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/')
   getHello(): string {
+    console.log('aaaaa');
     return this.appService.getHello();
   }
 }
